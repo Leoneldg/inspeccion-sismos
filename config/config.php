8d3961891a863163527ebe32a9cbf691b259cf6f
MrLorh <Lorhender.rojas@gmail.com>
2026-07-03 13:08:45 -0400

    c

--- config/config.php
+++ config/config.php
@@ -11,7 +11,7 @@ define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_NAME', getenv('DB_NAME') ?: 'inspecciones_sismos');
 define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_PASS', getenv('DB_PASS') ?: 'root');
 
 // ---------------------------------------------------------------------
 // Aplicación
